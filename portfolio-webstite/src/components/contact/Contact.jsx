52314c833add3d5f0b71ddb6dbb7087381aaad69
Jayesh Channe <87438326+Jayesh242663@users.noreply.github.com>
2025-01-28 21:10:47 +0530

    Update Contact.jsx
    Fixed the issue of email not sent

--- portfolio-webstite/src/components/contact/Contact.jsx
+++ portfolio-webstite/src/components/contact/Contact.jsx
@@ -1,3 +1,106 @@
+// import "./contact.css";
+// import emailjs from "@emailjs/browser";
+// import { useRef, useState } from "react";
+// import { motion, useInView } from "motion/react";
+// import ContactSvg from "./ContactSvg";
+
+// const listVariant = {
+//   initial: {
+//     x: 100,
+//     opacity: 0,
+//   },
+//   animate: {
+//     x: 0,
+//     opacity: 1,
+//     transition: {
+//       duration: 0.5,
+//       staggerChildren: 0.2,
+//     },
+//   },
+// };
+
+// const Contact = () => {
+//   const [success, setSuccess] = useState(false);
+//   const [error, setError] = useState(false);
+
+//   const ref = useRef();
+//   const form = useRef();
+
+//   const sendEmail = (e) => {
+//     e.preventDefault();
+
+//     emailjs
+//   .sendForm(
+//     import.meta.env.VITE_SERVICE_ID,
+//     import.meta.env.VITE_TEMPLATE_ID,
+//     form.current,
+//     {
+//       publicKey: import.meta.env.VITE_PUBLIC_KEY,
+//     }
+//   )
+//   .then(
+//     () => {
+//       setSuccess(true);
+//       setError(false);
+//     },
+//     (error) => {
+//       console.error("EmailJS Error:", error); // Log the entire error object
+//       setError(true);
+//       setSuccess(false);
+//     }
+//   );
+
+//   };
+
+//   const isInView = useInView(ref, { margin: "-200px" });
+
+//   return (
+//     <div className="contact" ref={ref} onSubmit={sendEmail}>
+//       <div className="cSection">
+//         <motion.form
+//           ref={form}
+//           variants={listVariant}
+//           animate={isInView ? "animate" : "initial"}
+//         >
+//           <motion.h1 variants={listVariant} className="cTitle">
+//             Let's keep in touch
+//           </motion.h1>
+//           <motion.div variants={listVariant} className="formItem">
+//             <label>Name</label>
+//             <input type="text" name="user_username" placeholder="Chetan Nanda" />
+//           </motion.div>
+//           <motion.div variants={listVariant} className="formItem">
+//             <label>Email</label>
+//             <input
+//               type="email"
+//               name="user_email"
+//               placeholder="chetannanda62@gmail.com"
+//             />
+//           </motion.div>
+//           <motion.div variants={listVariant} className="formItem">
+//             <label>Message</label>
+//             <textarea
+//               rows={10}
+//               name="user_message"
+//               placeholder="Write your message..."
+//             ></textarea>
+//           </motion.div>
+//           <motion.button variants={listVariant} className="formButton">
+//             Send
+//           </motion.button>
+//           {success && <span>Your message has been sent!</span>}
+//           {error && <span>Your message has been sent!</span>}
+//         </motion.form>
+//       </div>
+//       <div className="cSection"><ContactSvg/></div>
+//     </div>
+//   );
+// };
+
+// export default Contact;
+
+
+
 import "./contact.css";
 import emailjs from "@emailjs/browser";
 import { useRef, useState } from "react";
@@ -34,22 +137,24 @@ const Contact = () => {
     import.meta.env.VITE_SERVICE_ID,
     import.meta.env.VITE_TEMPLATE_ID,
     form.current,
-    {
-      publicKey: import.meta.env.VITE_PUBLIC_KEY,
-    }
+    import.meta.env.VITE_PUBLIC_KEY
   )
   .then(
     () => {
+      console.log("Email sent successfully");
       setSuccess(true);
       setError(false);
     },
     (error) => {
-      console.error("EmailJS Error:", error); // Log the entire error object
+      console.error("Error sending email:", error.text || error);
+      alert(error)
       setError(true);
       setSuccess(false);
     }
   );
 
+
+  
   };
 
   const isInView = useInView(ref, { margin: "-200px" });
@@ -74,7 +179,7 @@ const Contact = () => {
             <input
               type="email"
               name="user_email"
-              placeholder="chetannanda62@gmail.com"
+              placeholder="chetan@gmail.com"
             />
           </motion.div>
           <motion.div variants={listVariant} className="formItem">
@@ -89,7 +194,7 @@ const Contact = () => {
             Send
           </motion.button>
           {success && <span>Your message has been sent!</span>}
-          {error && <span>Your message has been sent!</span>}
+          {error && <span>Something went wrong!</span>}
         </motion.form>
       </div>
       <div className="cSection"><ContactSvg/></div>
